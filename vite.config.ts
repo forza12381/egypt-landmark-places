30159a1bcb52c1c11d2bd86f0f157ac1951aa836
[ziad4347] <71031166+ziad4347@users.noreply.github.com>
2026-01-29 04:56:24 +0200

    dd

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      base:process.env.VITE_BASE_PATH || '/Egypt-Landmark',
+      // server: {
+      //   port: 3000,
+      //   host: '0.0.0.0',
+      // },
       plugins: [react()],
+      base:process.env.VITE_BASE_PATH || '/Egypt-Landmark',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
